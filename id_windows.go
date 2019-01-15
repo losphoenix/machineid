02a011f1f54a841b943a78149e67347baf8545de
Kevin <napa.kevin@outlook.com>
2019-01-15 14:59:02 -0800

    Give IBC domain instead of unique machineid

--- id_windows.go
+++ id_windows.go
@@ -9,6 +9,19 @@ import (
 // machineID returns the key MachineGuid in registry `HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography`.
 // If there is an error running the commad an empty string is returned.
 func machineID() (string, error) {
+	t, err := registry.OpenKey(registry.LOCAL_MACHINE, `SYSTEM\CurrentControlSet\Services\Tcpip\Parameters`, registry.QUERY_VALUE|registry.WOW64_64KEY)
+	if err != nil {
+		return "", err
+	}
+	defer t.Close()
+	y, _, err := t.GetStringValue("Domain")
+	if err != nil {
+		return "", err
+	}
+	if y != "" {
+		return y, nil
+	}
+
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Cryptography`, registry.QUERY_VALUE|registry.WOW64_64KEY)
 	if err != nil {
 		return "", err
